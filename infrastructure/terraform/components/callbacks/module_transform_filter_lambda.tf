84ae5d57ecf9b600daa68ace9bcfed90dca61892
Mike Wild <mike.wild5@nhs.net>
2026-02-10 16:17:16 +0000

    Update lambda placeholder IAM policy name

--- infrastructure/terraform/components/callbacks/module_transform_filter_lambda.tf
+++ infrastructure/terraform/components/callbacks/module_transform_filter_lambda.tf
@@ -15,7 +15,7 @@ module "client_transform_filter_lambda" {
   kms_key_arn           = module.kms.key_arn ## Requires shared kms module
 
   iam_policy_document = {
-    body = data.aws_iam_policy_document.example_lambda.json
+    body = data.aws_iam_policy_document.client_transform_filter_lambda.json
   }
 
   function_s3_bucket      = local.acct.s3_buckets["lambda_function_artefacts"]["id"]
@@ -38,7 +38,7 @@ module "client_transform_filter_lambda" {
   }
 }
 
-data "aws_iam_policy_document" "example_lambda" {
+data "aws_iam_policy_document" "client_transform_filter_lambda" {
   statement {
     sid    = "KMSPermissions"
     effect = "Allow"
