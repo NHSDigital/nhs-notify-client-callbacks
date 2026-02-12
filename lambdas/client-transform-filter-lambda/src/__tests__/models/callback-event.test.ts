11c6c81ab41525811f6e4da704d66bee05e45d34
Mike Wild <mike.wild5@nhs.net>
2026-02-12 09:49:28 +0000

    Placeholder tests to test coverage

---
+++ lambdas/client-transform-filter-lambda/src/__tests__/models/callback-event.test.ts
@@ -0,0 +1,13 @@
+import { EventTypes } from "models/callback-event";
+
+// coverage purposes
+describe("EventTypes", () => {
+  it("should match the expected event type values", () => {
+    expect(EventTypes).toEqual({
+      MESSAGE_STATUS_TRANSITIONED:
+        "uk.nhs.notify.client-callbacks.message.status.transitioned.v1",
+      CHANNEL_STATUS_TRANSITIONED:
+        "uk.nhs.notify.client-callbacks.channel.status.transitioned.v1",
+    });
+  });
+});
